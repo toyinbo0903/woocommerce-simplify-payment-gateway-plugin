e98817b153d6b7309c49edfae6bc0462268ab4f5
toyinbo0903 <140073857+toyinbo0903@users.noreply.github.com>
2024-07-05 19:21:26 +0000

    lop

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '2.4.0',
+        'version' => '2.4.0.0',
+        'reference' => NULL,
+        'type' => 'woocommerce-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '2.4.0',
+            'version' => '2.4.0.0',
+            'reference' => NULL,
+            'type' => 'woocommerce-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
